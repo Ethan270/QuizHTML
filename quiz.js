ecb68f8fb8ee41010447050fd804d176ac07e239
[Ethan Gonzalez] <[ethang27000@gmail.com]>
2024-10-06 23:20:28 -0500

    First commit

---
+++ quiz.js
@@ -0,0 +1,29 @@
+/* var score = 0;
+var TriviaQuestions=[
+["How many dots are on each Pacman board?",240],
+["How tall is the Statue of Liberty Sculpture in Feet?",305],
+[]
+
+
+
+
+];
+function askQuestion(TriviaQuestions){
+    var answer = prompt(TriviaQuestions[0],'');
+    if(answer == question[1]) {
+        alert('Correct!');
+        score++; 
+    } else {
+        alert('Sorry. The correct answer is' + TriviaQuestions[1]);
+    }
+
+}
+for (var i=0; i<TriviaQuestions.length; i++){
+    askQuestion(TriviaQuestions[i]);
+
+}
+
+var message= 'You got' + score;
+message += ' out of' + TriviaQuestions.length;
+message += ' questions correct.';
+document.write('<p>' + message + '</p>'); */
